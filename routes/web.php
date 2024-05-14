a412128bab435c6d6bb8ab147cbe4105b2c15433
Kevin Lain Sierra <49158887+klsierraj@users.noreply.github.com>
2024-05-14 18:44:09 -0500

    Setting login and logout

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [UserController::class, "showCorrectHomepage"]);
+
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
